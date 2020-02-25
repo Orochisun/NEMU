29f755b382d6244f8f52638e588a6dece22b648e
tracer-ics2019 <tracer@njuics.org>
2020-02-25 20:42:11 +0800

    >  compile 123 ssj Linux iZ2ze4ib9215tde4tzqnrdZ 4.4.0-170-generic #199-Ubuntu SMP Thu Nov 14 01:45:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:42:11 up 6 days,  8:12,  0 users,  load average: 1.19, 0.42, 0.15 ac11d201dc8fc8d273a2260e9277fc62ac2ccee6

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,9 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "monitor/expr.h"
+
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
+  
+  // test expr
+  if (argc < 2) {
+    printf("usage\n");
+    return 0;
+  }
+  FILE *fp;
+  fp = fopen(argv[1], "r");
+  char buf[101];
+  while(fgets(buf, sizeof(buf), fp)){
+    char *p = strtok(buf, " ");
+    char *q = strtok(NULL, "\n");
+    bool success = true;
+    int res = expr(q, &success);
+    if(success) {
+      if(atoi(p) == res) {
+        printf("%s = %s, myresult=%d, equal=true\n", q, p, res);
+      }
+      else {
+        printf("%s = %s, myresult=%d, equal=false\n", q, p, res);
+      }
+    }
+    else {
+      printf("fail to eval");
+    }
+  } 
+  fclose(fp);
 
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
