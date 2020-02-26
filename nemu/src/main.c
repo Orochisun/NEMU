c3eb4688aadf8b28648dcfa89499696f8aaa07d1
tracer-ics2019 <tracer@njuics.org>
2020-02-26 23:59:46 +0800

    >  compile 123 ssj Linux iZ2ze4ib9215tde4tzqnrdZ 4.4.0-170-generic #199-Ubuntu SMP Thu Nov 14 01:45:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  23:59:46 up 7 days, 11:29,  0 users,  load average: 0.18, 0.15, 0.06 d224803369d5554f3b0d7dc97baf95e039d86e81

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -11,27 +11,27 @@ int main(int argc, char *argv[]) {
   int is_batch_mode = init_monitor(argc, argv);
   
   // test expr
-  FILE *fp;
-  fp = fopen("/home/ssj/ics2019/nemu/tools/gen-expr/input", "r");
-  char buf[101];
-  while(fgets(buf, sizeof(buf), fp)){
-    char *p = strtok(buf, " ");
-    char *q = strtok(NULL, "\n");
-    bool success = true;
-    int res = expr(q, &success);
-    if(success) {
-      if(atoi(p) == res) {
-        printf("%s = %s, myresult=%d, equal=true\n", q, p, res);
-      }
-      else {
-        printf("%s = %s, myresult=%d, equal=false\n", q, p, res);
-      }
-    }
-    else {
-      printf("fail to eval");
-    }
-  } 
-  fclose(fp);
+  // FILE *fp;
+  // fp = fopen("/home/ssj/ics2019/nemu/tools/gen-expr/input", "r");
+  // char buf[101];
+  // while(fgets(buf, sizeof(buf), fp)){
+  //   char *p = strtok(buf, " ");
+  //   char *q = strtok(NULL, "\n");
+  //   bool success = true;
+  //   int res = expr(q, &success);
+  //   if(success) {
+  //     if(atoi(p) == res) {
+  //       printf("%s = %s, myresult=%d, equal=true\n", q, p, res);
+  //     }
+  //     else {
+  //       printf("%s = %s, myresult=%d, equal=false\n", q, p, res);
+  //     }
+  //   }
+  //   else {
+  //     printf("fail to eval");
+  //   }
+  // } 
+  // fclose(fp);
 
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
