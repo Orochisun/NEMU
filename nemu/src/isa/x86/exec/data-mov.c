0a1636ca1f764ebf99e828d5443139e9608ebe2b
tracer-ics2019 <tracer@njuics.org>
2020-03-01 23:26:43 +0800

    >  compile 123 ssj Linux iZ2ze4ib9215tde4tzqnrdZ 4.4.0-170-generic #199-Ubuntu SMP Thu Nov 14 01:45:04 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  23:26:43 up 11 days, 10:56,  0 users,  load average: 0.00, 0.02, 0.00 bceaf8cd701c1196f2587913372d8493879bb01b

--- nemu/src/isa/x86/exec/data-mov.c
+++ nemu/src/isa/x86/exec/data-mov.c
@@ -19,35 +19,49 @@ make_EHelper(pop) {
 }
 
 make_EHelper(pusha) {
-  TODO();
+  // TODO();
+  s0 = cpu.esp;
+  rtl_push(&cpu.eax);
+  rtl_push(&cpu.ecx);
+  rtl_push(&cpu.edx);
+  rtl_push(&cpu.ebx);
+  rtl_push(&s0);
+  rtl_push(&cpu.ebp);
+  rtl_push(&cpu.esi);
+  rtl_push(&cpu.edi);
 
   print_asm("pusha");
 }
 
 make_EHelper(popa) {
-  TODO();
-
+  // TODO();
+  rtl_pop(&cpu.edi);
+  rtl_pop(&cpu.esi);
+  rtl_pop(&cpu.ebp);
+  rtl_pop(&s0);
+  rtl_pop(&cpu.ebx);
+  rtl_pop(&cpu.edx);
+  rtl_pop(&cpu.ecx);
+  rtl_pop(&cpu.eax);
   print_asm("popa");
 }
 
-make_EHelper(pushl) {
-  TODO();
-
-  print_asm("pushl");
-}
-
 make_EHelper(leave) {
-  TODO();
-
+  // TODO();
+  rtl_mv(&cpu.esp, &cpu.ebp);
+  rtl_pop(&cpu.ebp);
   print_asm("leave");
 }
 
 make_EHelper(cltd) {
   if (decinfo.isa.is_operand_size_16) {
-    TODO();
+    // TODO();
+    rtl_sext(&s0, &reg_l(R_EAX), 2);
+    rtl_shri(&reg_l(R_EDX), &s0, 16);
   }
   else {
-    TODO();
+    // TODO();
+    rtl_sari(&reg_l(R_EDX), &reg_l(R_EAX), 31);
   }
 
   print_asm(decinfo.isa.is_operand_size_16 ? "cwtl" : "cltd");
@@ -55,10 +69,14 @@ make_EHelper(cltd) {
 
 make_EHelper(cwtl) {
   if (decinfo.isa.is_operand_size_16) {
-    TODO();
+    // TODO();
+    rtl_shli(&reg_l(R_EAX), &reg_l(R_EAX), 24);
+    rtl_sari(&reg_l(R_EAX), &reg_l(R_EAX), 8);
+    rtl_shri(&reg_l(R_EAX), &reg_l(R_EAX), 16);
   }
   else {
-    TODO();
+    // TODO();
+    rtl_sext(&reg_l(R_EAX), &reg_l(R_EAX), 2);
   }
 
   print_asm(decinfo.isa.is_operand_size_16 ? "cbtw" : "cwtl");
